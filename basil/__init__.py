57105c637d6e28a91627069c181bd8567af5d436
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-03-19 15:30:33 +0000

    Remove old modelling widget and structure-only widget

--- basil/__init__.py
+++ basil/__init__.py
@@ -6,12 +6,11 @@ Copyright (c) 2016-2017 University of Oxford, Martin Craig
 """
 from quantiphyse.utils import get_local_shlib
 
-from .widget import ASLWidget
 from .widgets import AslStrucCheck, AslDataWidget, AslPreprocWidget, AslBasilWidget
 from .process import AslDataProcess, AslPreprocProcess
 
 QP_MANIFEST = {
-    "widgets" : [ASLWidget, AslDataWidget, AslPreprocWidget, AslBasilWidget],
+    "widgets" : [AslPreprocWidget, AslBasilWidget],
     "fabber-libs" : [get_local_shlib("fabber_models_asl", __file__)],
     "asl-widgets" : [AslStrucCheck],
 }
