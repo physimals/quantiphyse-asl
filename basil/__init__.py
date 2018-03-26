675696fb4104850b81de555f2ca2c880f2f19c5a
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-03-26 11:47:39 +0100

    Register the ASL data structure widget so other plugins can use it

--- basil/__init__.py
+++ basil/__init__.py
@@ -6,11 +6,11 @@ Copyright (c) 2016-2017 University of Oxford, Martin Craig
 """
 from quantiphyse.utils import get_local_shlib
 
-from .widgets import AslStrucCheck, AslDataWidget, AslPreprocWidget, AslBasilWidget
+from .widgets import AslStrucCheck, AslStrucWidget, AslDataWidget, AslPreprocWidget, AslBasilWidget
 from .process import AslDataProcess, AslPreprocProcess
 
 QP_MANIFEST = {
     "widgets" : [AslPreprocWidget, AslBasilWidget],
     "fabber-libs" : [get_local_shlib("fabber_models_asl", __file__)],
-    "asl-widgets" : [AslStrucCheck],
+    "asl-widgets" : [AslStrucCheck, AslStrucWidget],
 }
