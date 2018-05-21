38806931c23ca2e43efbb48688c717d234cf3406
Martin Craig <martin.craig@eng.ox.ac.uk>
2018-05-21 15:29:06 +0100

    Some initial tests added

--- basil/__init__.py
+++ basil/__init__.py
@@ -8,10 +8,13 @@ from quantiphyse.utils import get_local_shlib
 
 from .widgets import AslStrucCheck, AslStrucWidget, AslDataWidget, AslPreprocWidget, AslBasilWidget, AslCalibWidget, AslMultiphaseWidget
 from .process import AslDataProcess, AslPreprocProcess, BasilProcess, AslMultiphaseProcess
+from .tests import AslPreprocWidgetTest, MultiphaseProcessTest, BasilProcessTest
 
 QP_MANIFEST = {
     "widgets" : [AslPreprocWidget, AslBasilWidget, AslCalibWidget, AslMultiphaseWidget],
     "fabber-libs" : [get_local_shlib("fabber_models_asl", __file__)],
     "asl-widgets" : [AslStrucCheck, AslStrucWidget],
     "processes" : [BasilProcess, AslMultiphaseProcess],
+    "widget-tests" : [AslPreprocWidgetTest],
+    "process-tests" : [MultiphaseProcessTest, BasilProcessTest],
 }
---
+++ basil/tests.py
@@ -0,0 +1,287 @@
+"""
+Quantiphyse - Tests for ASL widgets
+
+Copyright (c) 2013-2018 University of Oxford
+"""
+import sys
+import unittest 
+
+import numpy as np
+
+from quantiphyse.processes import Process
+from quantiphyse.test import WidgetTest, ProcessTest
+
+from .widgets import AslPreprocWidget, LabelType, DataOrdering, ORDER_LABELS
+from .process import AslMultiphaseProcess
+
+class AslPreprocWidgetTest(WidgetTest):
+
+    def widget_class(self):
+        return AslPreprocWidget
+
+    def testNoData(self):
+        """ User clicks the generate buttons with no data"""
+        self.harmless_click(self.w.run_btn)
+
+    def test3dDataNoPreproc(self):
+        self.ivm.add_data(self.data_3d, grid=self.grid, name="data_3d")
+        self.w.struc_widget.set_data_name("data_3d")
+        self.processEvents()
+        self.assertFalse(self.error)
+        
+        # 3D data so set to already differenced
+        label_type_widget = self._struc_widget(LabelType)
+        label_type_widget.combo.setCurrentIndex(2)
+        self.processEvents()
+
+        struc = self.ivm.data["data_3d"].metadata.get("AslData", None)
+        self.assertTrue(struc is not None)
+        self.assertEqual(len(struc["tis"]), 1)
+        self.assertTrue("p" not in struc["order"])
+        self.assertTrue("P" not in struc["order"])
+        self.assertTrue("m" not in struc["order"])
+        
+        self.harmless_click(self.w.run_btn)
+        self.processEvents()
+
+        # Nothing done, so no new data
+        self.assertEqual(len(self.ivm.data), 1)
+        self.assertTrue("data_3d" in self.ivm.data)
+
+    def test4dDataNoPreproc(self):
+        """
+        Check a single-ti data set with no preprocessing
+        """
+        self.ivm.add_data(self.data_4d, grid=self.grid, name="data_4d")
+        self.w.struc_widget.set_data_name("data_4d")
+        self.processEvents()
+        self.assertFalse(self.error)
+    
+        # Treat data as TC pairs
+        label_type_widget = self._struc_widget(LabelType)
+        label_type_widget.combo.setCurrentIndex(0)
+        self.processEvents()
+
+        struc = self.ivm.data["data_4d"].metadata.get("AslData", None)
+        self.assertTrue(struc is not None)
+        self.assertEqual(len(struc["tis"]), 1)
+        self.assertTrue("p" in struc["order"])
+        self.assertTrue("P" not in struc["order"])
+        self.assertTrue("m" not in struc["order"])
+        
+        self.harmless_click(self.w.run_btn)
+        self.processEvents()
+
+        # Nothing done, so no new data
+        self.assertEqual(len(self.ivm.data), 1)
+        self.assertTrue("data_4d" in self.ivm.data)
+
+    def testDiff(self):
+        """
+        Check a single-ti data set with tag-control differencing
+        """
+        self.ivm.add_data(self.data_4d, grid=self.grid, name="data_4d")
+        self.w.struc_widget.set_data_name("data_4d")
+        self.processEvents()
+        self.assertFalse(self.error)
+        
+        # Treat data as TC pairs
+        label_type_widget = self._struc_widget(LabelType)
+        label_type_widget.combo.setCurrentIndex(0)
+        self.processEvents()
+
+        # Select tag-control differencing
+        self.w.sub_cb.setChecked(True)
+        self.processEvents()
+        
+        self.harmless_click(self.w.run_btn)
+        self.processEvents()
+
+        self.assertEqual(len(self.ivm.data), 2)
+        self.assertTrue("data_4d" in self.ivm.data)
+        self.assertTrue("data_4d_diff" in self.ivm.data)
+
+        # Check shape is as expected
+        diffdata = self.ivm.data["data_4d_diff"]
+        shape = list(self.data_4d.shape)
+        shape[3] = shape[3] / 2
+        for idx, n in enumerate(shape):
+            self.assertEqual(diffdata.raw().shape[idx], n)
+
+        # Check struct is as expected
+        struc = diffdata.metadata.get("AslData", None)
+        self.assertTrue(struc is not None)
+        self.assertEqual(len(struc["tis"]), 1)
+        self.assertTrue("p" not in struc["order"])
+        self.assertTrue("P" not in struc["order"])
+        self.assertTrue("m" not in struc["order"])
+        
+        # Check data is as expected (control - tag)
+        diffdata_test = np.zeros(shape)
+        for v in range(shape[3]):
+            diffdata_test[..., v] = self.data_4d[..., v*2+1] - self.data_4d[..., v*2]
+        self.assertTrue(np.all(diffdata_test == diffdata.raw()))
+
+    def testMean(self):
+        """
+        Check a single-ti data set with the 'mean across repeats option
+        """
+        self.ivm.add_data(self.data_4d, grid=self.grid, name="data_4d")
+        self.w.struc_widget.set_data_name("data_4d")
+        self.processEvents()
+        self.assertFalse(self.error)
+        
+        # Treat data as TC pairs
+        label_type_widget = self._struc_widget(LabelType)
+        label_type_widget.combo.setCurrentIndex(0)
+        self.processEvents()
+
+        # Select mean across repeats
+        self.w.mean_cb.setChecked(True)
+        self.processEvents()
+        
+        self.harmless_click(self.w.run_btn)
+        self.processEvents()
+
+        self.assertEqual(len(self.ivm.data), 2)
+        self.assertTrue("data_4d" in self.ivm.data)
+        self.assertTrue("data_4d_mean" in self.ivm.data)
+
+        # Check shape is as expected
+        meandata = self.ivm.data["data_4d_mean"]
+        shape = list(self.data_4d.shape[:3])
+        for idx, n in enumerate(shape):
+            self.assertEqual(meandata.raw().shape[idx], n)
+
+        # Check struct is as expected
+        struc = meandata.metadata.get("AslData", None)
+        self.assertTrue(struc is not None)
+        self.assertEqual(len(struc["tis"]), 1)
+        self.assertTrue("p" not in struc["order"])
+        self.assertTrue("P" not in struc["order"])
+        self.assertTrue("m" not in struc["order"])
+        
+        # Check data is as expected (differenced, then mean across repeats)
+        meandata_test = np.zeros(shape)
+        diffdata_test = np.zeros(list(shape) + [self.data_4d.shape[3] / 2])
+        for v in range(diffdata_test.shape[3]):
+            diffdata_test[..., v] = self.data_4d[..., v*2+1] - self.data_4d[..., v*2]
+        meandata_test = np.mean(diffdata_test, axis=-1)
+        self.assertTrue(np.allclose(meandata_test, meandata.raw()))
+
+    def testReorder(self):
+        """
+        Check a single-ti data set with reordering to 'all tags' then 'all controls'
+        """
+        self.ivm.add_data(self.data_4d, grid=self.grid, name="data_4d")
+        self.w.struc_widget.set_data_name("data_4d")
+        self.processEvents()
+        self.assertFalse(self.error)
+        
+        # Treat data as TC pairs in order prt (top = outermost)
+        label_type_widget = self._struc_widget(LabelType)
+        label_type_widget.combo.setCurrentIndex(0)
+        data_order_widget = self._struc_widget(DataOrdering)
+        data_order_widget.group_list.setItems([ORDER_LABELS[g][2] for g in "trp"])
+        self.processEvents()
+
+        # Select reorder
+        self.w.reorder_cb.setChecked(True)
+        self.w.new_order.setText("trp")
+        self.processEvents()
+        
+        self.harmless_click(self.w.run_btn)
+        self.processEvents()
+
+        self.assertEqual(len(self.ivm.data), 2)
+        self.assertTrue("data_4d" in self.ivm.data)
+        self.assertTrue("data_4d_reorder" in self.ivm.data)
+
+        # Check shape is as expected
+        reordered_data = self.ivm.data["data_4d_reorder"]
+        shape = list(self.data_4d.shape)
+        for idx, n in enumerate(shape):
+            self.assertEqual(reordered_data.raw().shape[idx], n)
+
+        # Check struct is as expected
+        struc = reordered_data.metadata.get("AslData", None)
+        self.assertTrue(struc is not None)
+        self.assertEqual(len(struc["tis"]), 1)
+        self.assertEqual(struc["order"], "trp")
+        
+        # Check data is as expected (all tags first, then all controls)
+        reordered_test = np.zeros(shape)
+        for v in range(shape[3]/2):
+            reordered_test[..., v] = self.data_4d[..., 2*v]
+            reordered_test[..., v+shape[3]/2] = self.data_4d[..., 2*v+1]
+        self.assertTrue(np.allclose(reordered_test, reordered_data.raw()))
+
+    def _struc_widget(self, cls):
+        for view in self.w.struc_widget.views:
+            if isinstance(view, cls):
+                return view
+
+class MultiphaseProcessTest(ProcessTest):
+
+    @unittest.skipIf("--fast" in sys.argv, "Slow test")
+    def testJLarkin(self):
+        """
+        Test on James Larkin's multiphase data. Note that this data is not currently
+        bundled with Quantiphyse so test will fail if not present
+        """
+        yaml = """
+  - Load:
+      data:
+        /home/ibmeuser/data/asl/jlarkin/TagDurations/TD=1.4/asl_phase_shifted_5_OPTIMAL/asl_phase_shifted_5.nii: multiphase_data
+      rois:
+        /home/ibmeuser/data/asl/jlarkin/TagDurations/TD=1.4/asl_phase_shifted_5_OPTIMAL/mask.nii.gz: multiphase_mask
+
+  - AslMultiphase:
+      data: multiphase_data
+      roi : multiphase_mask
+      nphases: 8
+      keep-temp: True
+      sigma: 1
+      n-supervoxels: 8
+      compactness: 0.1
+"""
+        self.run_yaml(yaml)
+        self.assertEqual(self.status, Process.SUCCEEDED)
+        self.assertTrue("mean_mag" in self.ivm.data)
+        self.assertTrue("mean_offset" in self.ivm.data)
+        self.assertTrue("mean_phase" in self.ivm.data)
+
+class BasilProcessTest(ProcessTest):
+
+    @unittest.skipIf("--fast" in sys.argv, "Slow test")
+    def testFslCourse(self):
+        """
+        Basil BASIL process test. Note that this data is not currently
+        bundled with Quantiphyse so test will fail if not present
+        """
+        yaml = """
+  - Load:
+      data:
+        /home/ibmeuser/data/asl/fsl_course/ASL/mpld_asltc.nii.gz: asldata
+      rois:
+        /home/ibmeuser/data/asl/fsl_course/ASL/mask.nii.gz: aslmask
+
+  - Basil:
+      data: asldata
+      roi : aslmask
+      order: prt
+      tis: [0.25, 0.5, 0.75, 1.0, 1.25, 1.5]
+      casl: True
+      taus: [1.4, 1.4, 1.4, 1.4, 1.4, 1.4]
+      infertiss: True
+"""
+        self.run_yaml(yaml)
+        self.assertEqual(self.status, Process.SUCCEEDED)
+        self.assertTrue("perfusion" in self.ivm.data)
+        self.assertTrue("perfusion_std" in self.ivm.data)
+        self.assertTrue("arrival" in self.ivm.data)
+        self.assertTrue("arrival_std" in self.ivm.data)
+        
+
+if __name__ == '__main__':
+    unittest.main()
