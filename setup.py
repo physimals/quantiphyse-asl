1343805d3ab2fc597e199a5119567960d1fc085c
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-01-17 11:31:29 +0000

    Fleshed out setup script with a view to PyPi upload

--- setup.py
+++ setup.py
@@ -1,31 +1,71 @@
 #!/usr/bin/env python
+import os
+import subprocess
+import re
+import sys
+
 from setuptools import setup
+from setuptools import find_packages
 
-desc = "Quantiphyse plugin for ASL"
-version = "0.0.1"
+kwargs = {
+    'name' : 'qp-basil',
+    'description' : 'Quantiphyse plugin for ASL-MRI data',
+    'url' : 'https://quantiphyse.readthedocs.io/',
+    'author' : 'Martin Craig',
+    'author_email' : 'martin.craig@eng.ox.ac.uk',
+    'license' : '',
+}
 
-# setup parameters
-setup(name='qp-basil',
-      cmdclass={},
-      version=version,
-      description=desc,
-      long_description=desc,
-      author='Michael Chappell, Martin Craig',
-      author_email='martin.craig@eng.ox.ac.uk',
-      packages=['basil'],
-      include_package_data=True,
-      data_files=[],
-      setup_requires=[],
-      install_requires=[],
-      classifiers=["Programming Language :: Python :: 2.7",
-                   "Development Status:: 3 - Alpha",
-                   'Programming Language :: Python',
-                   'Operating System :: MacOS :: MacOS X',
-                   'Operating System :: Microsoft :: Windows',
-                   'Operating System :: POSIX',
-                   "Intended Audience :: Education",
-                   "Intended Audience :: Science/Research",
-                   "Intended Audience :: End Users/Desktop",
-                   "Topic :: Scientific/Engineering :: Bio-Informatics",],
-     )
-     
+def git_version():
+    # Full version includes the Git commit hash
+    full_version = subprocess.check_output('git describe --dirty', shell=True).decode("utf-8").strip(" \n")
+
+    # Python standardized version in form major.minor.patch.dev<build>
+    version_regex = re.compile(r"v?(\d+\.\d+\.\d+(-\d+)?).*")
+    match = version_regex.match(full_version)
+    if match:
+        std_version = match.group(1).replace("-", ".dev")
+    else:
+        raise RuntimeError("Failed to parse version string %s" % full_version)
+
+    return full_version, std_version
+
+def git_timestamp():
+    return subprocess.check_output('git log -1 --format=%cd', shell=True).decode("utf-8").strip(" \n")
+
+def set_metadata(module_dir, version_str, timestamp_str):
+    vfile = open(os.path.join(module_dir, "basil", "_version.py"), "w")
+    vfile.write("__version__ = '%s'\n" % version_str)
+    vfile.write("__timestamp__ = '%s'\n" % timestamp_str)
+    vfile.close()
+
+def get_lib_template():
+    if sys.platform.startswith("win"):
+        return "bin", "%s.dll"
+    elif sys.platform.startswith("darwin"):
+        return "lib", "lib%s.dylib"
+    else:
+        return "lib", "lib%s.so"
+
+# Read in requirements from the requirements.txt file.
+with open('requirements.txt', 'rt') as f:
+    requirements = [l.strip() for l in f.readlines()]
+
+rootdir = os.path.abspath(os.path.dirname(__file__))
+_, stdv = git_version()
+timestamp = git_timestamp()
+set_metadata(rootdir, stdv, timestamp)
+
+setup(
+    packages=find_packages(),
+    version=stdv,
+    install_requires=requirements,
+    entry_points={
+        'quantiphyse_plugins' : [
+            "basil = basil:QP_MANIFEST",
+        ],
+    },
+    classifiers=[
+    ],
+    **kwargs
+)
